a5a644e5286b0bb87e9c533c1a8c62b640cab152
CPoet <llzero54@foxmail.com>
2025-12-21 18:02:55 +0800

    feat: Patch generation supports file selection from VCS commit records

--- core/src/main/java/cn/cpoet/tool/actions/patch/GenPatchPackageAction.java
+++ core/src/main/java/cn/cpoet/tool/actions/patch/GenPatchPackageAction.java
@@ -7,6 +7,13 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogBuilder;
+import com.intellij.openapi.vcs.VcsDataKeys;
+import com.intellij.openapi.vcs.changes.Change;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 生成补丁包（适用于增量发包的情况）
@@ -18,7 +25,7 @@ public class GenPatchPackageAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         Project project = e.getRequiredData(CommonDataKeys.PROJECT);
-        Object[] selectedItems = e.getData(LangDataKeys.SELECTED_ITEMS);
+        Object[] selectedItems = getSelectedItems(e);
         DialogBuilder dialogBuilder = new DialogBuilder(project);
         GenPatchPanel packagePanel = new GenPatchPanel(project, selectedItems, dialogBuilder.getDialogWrapper());
         dialogBuilder.setTitle(I18nUtil.t("actions.patch.GenPatchPackageAction.title"));
@@ -28,5 +35,29 @@ public class GenPatchPackageAction extends AnAction {
         dialogBuilder.setOkOperation(packagePanel::generate);
         dialogBuilder.addCancelAction().setText(I18nUtil.t("actions.patch.GenPatchPackageAction.cancel"));
         dialogBuilder.showNotModal();
+    }
+
+    private Object[] getSelectedItems(AnActionEvent e) {
+        Object[] items = e.getData(LangDataKeys.SELECTED_ITEMS);
+        Change[] changes = e.getData(VcsDataKeys.SELECTED_CHANGES);
+        if (items == null && changes == null) {
+            return null;
+        }
+        if (items != null && changes != null) {
+            List<Object> selectedItems = new ArrayList<>(items.length + changes.length);
+            Collections.addAll(selectedItems, items);
+            for (Change change : changes) {
+                selectedItems.add(change.getVirtualFile());
+            }
+            return selectedItems.toArray();
+        }
+        if (items != null) {
+            return items;
+        }
+        VirtualFile[] files = new VirtualFile[changes.length];
+        for (int i = 0; i < changes.length; ++i) {
+            files[i] = changes[i].getVirtualFile();
+        }
+        return files;
     }
 }
